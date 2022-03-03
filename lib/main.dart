513520b7c065d03f17e9b08692df944e0ffd3445
teitei-tk <teitei.tk@gmail.com>
2022-03-03 20:42:31 +0900

    Step 2: Use an external package
    https://docs.flutter.dev/get-started/codelab#step-2-use-an-external-package

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 // Copyright 2018 The Flutter team. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
+import "package:english_words/english_words.dart";
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,14 +13,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: Center(
+          child: Text(wordPair.asPascalCase),
         ),
       ),
     );
